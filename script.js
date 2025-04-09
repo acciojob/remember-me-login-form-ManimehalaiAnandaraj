627791f33b376c50c6b2b5a9daaca5138526bca3
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-04-09 14:46:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+	const loginForm =document.getElementById('existing');
+	const usernameInput=document.getElementById('username');
+	const passwordInput=document.getElementById('password');
+	const remembermeCheckbox=document.getElementById('checkbox');
+
+	if(localStorage.getItem('username'))
+	{
+		usernameInput.value=localStorage.getItem('username');
+		passwordInput.value=localStorage.getItem('password');
+		remembermeCheckbox.checked=true;
+	}
+
+	loginForm.addEventListener('submit',function (event) {
+		event.preventDefault();
+
+		if(remembermeCheckbox.checked){
+			localStorage.setItem('username',usernameInput.value);
+			localStorage.setItem('password',passwordInput.value);
+		}
+		else
+		{
+			localStorage.removeItem('username');
+			localStorage.removeItem('password');
+		
+		}
+		alert('Logged in Successfully');
+	});	
+});
